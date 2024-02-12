e4f11234d575377019233f4f7454b5b44d11891d
AW75020 <maelabdelhak@gmail.com>
2024-02-12 16:47:16 +0100

    ok

---
+++ js/script.js
@@ -0,0 +1,73 @@
+var btns = document.getElementsByClassName("btn");
+		for (var i = 0; i < btns.length; i++) {
+		btns[i].addEventListener("click", function() {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+		});
+		}
+		
+		function popupfunc1() {
+		var popup = document.getElementById("popup-li-1");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc2() {
+		var popup = document.getElementById("popup-li-2");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc3() {
+		var popup = document.getElementById("popup-li-3");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc4() {
+		var popup = document.getElementById("popup-li-4");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc5() {
+		var popup = document.getElementById("popup-li-5");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc6() {
+		var popup = document.getElementById("popup-li-6");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc7() {
+		var popup = document.getElementById("popup-li-7");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc8() {
+		var popup = document.getElementById("popup-li-8");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc9() {
+		var popup = document.getElementById("popup-li-9");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc10() {
+		var popup = document.getElementById("popup-li-10");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc11() {
+		var popup = document.getElementById("popup-li-11");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc12() {
+		var popup = document.getElementById("popup-li-12");
+		popup.classList.toggle("show");
+		}
+		
+		function popupfunc13() {
+		var popup = document.getElementById("popup-li-13");
+		popup.classList.toggle("show");
+		}
